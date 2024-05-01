14f3ec2eed8385b8e88f9261b8a9bfa9904971a9
grzegorzsol@gmail.com <grzegorzsol>
2024-05-01 06:05:54 +0200

    Dalsze prace nad klasą GsPyChartClass

--- GsPyChartClass/GsPyChartClass_example.py
+++ GsPyChartClass/GsPyChartClass_example.py
@@ -1,12 +1,12 @@
-#  Copyright (c) Grzegorz Sołtysik 29.04.2024, 06:52
+#  Copyright (c) Grzegorz Sołtysik 01.05.2024, 06:03
 #  Nazwa projektu: GsPyChartClass
 #  Nazwa pliku: GsPyChartClass_example.py
 
 from GsPyMyChart import GsPyMyChart, GsPyMyChartBox
 
 
-fTable = [0.2, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0]
+fTable = [0.78, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0]
 fTableM = [0.50, 1.1, 2.9, 3.3, 4.9, 5.5, 6.6, 7.7, 8.8, 9.1, 10.0]
 
-a = GsPyMyChart(fTable, fTableM, "test.pdf")
-b = GsPyMyChartBox(fTable, fTableM, "test1.pdf")
+a = GsPyMyChart(fTable, fTableM, "test.pdf", "Wykres liniowy błędów dla aparatu:")
+b = GsPyMyChartBox(fTable, fTableM, "test1.pdf", "Wykres słupkowy błędów dla aparatu:")
--- GsPyChartClass/GsPyMyChart.py
+++ GsPyChartClass/GsPyMyChart.py
@@ -1,4 +1,4 @@
-#  Copyright (c) Grzegorz Sołtysik 29.04.2024, 06:52
+#  Copyright (c) Grzegorz Sołtysik 01.05.2024, 06:03
 #  Nazwa projektu: GsPyChartClass
 #  Nazwa pliku: GsPyMyChart.py
 
@@ -9,17 +9,30 @@ import fpdf
 """
 class GsPyMyChartBase:
     # Konstruktor klasy bazowej
-    def __init__(self, fInTableRef: list, fInTableMeas: list):
+    def __init__(self, fInTableRef: list, fInTableMeas: list, strTitle: str):
         """
         @param: fInTableRef: Tablica wyników referencyjnych.
         @param: fInTableMeas: Tablica pomiarów.
         @return: Brak.
         """
         # print("Konstruktor klasy GsPyMyChartBase")
-        # Zmienne prywatne
+        # Zmienne protected
         self._iMaxMeas: int = 11
         self._fTableRef: list = fInTableRef.copy()
         self._fTableMeas: list = fInTableMeas.copy()
+        self._strTitle: str = strTitle
+        # Potrzebne obliczenia
+        self.__calculate()
+        # Tworzenie dokumentu PDF
+        self.__pdf_init()
+        # Rysowanie nagłówka
+        self.__paint_title()
+
+    # Obliczanie niezbędnych danych
+    def __calculate(self):
+        """
+        @return: Brak.
+        """
         # Zmienne publiczne
         self._iCountRef: int = len(self._fTableRef)
         # Wymiary w pikselach kartki A4
@@ -29,22 +42,29 @@ class GsPyMyChartBase:
         # Wymiary wykresu
         self._fWidth: float = self._fMaxWidth - (2 * self._fOffset)
         self._fHeight: float = self._fMaxHeight - (2 * self._fOffset)
-        __fReductionNet: float = 20  # O ile zostanie zmniejszony wykres w pionie i pozimie wzglądem wspólrzędnych
+        __fReductionNet: float = 20  # O ile zostanie zmniejszony wykres w pionie i poziomie względem współrzędnych.
         __fWidthOffset: float = self._fWidth - __fReductionNet  # Zmniejszenie siatki w poziomie
         __fheightOffset: float = self._fHeight - __fReductionNet  # Zmniejszenie siatki w pionie
+
         # Obliczanie współczynników konwersji
         self._fYConvRef: float = __fheightOffset / self._fTableRef[self._iCountRef - 1]  # Ilość pikseli na jednostkę, tablica wyników odniesienia, oś Y
         self._fX: float = __fWidthOffset / (self._iCountRef - 1)  # Ilość pikseli na kolejny pomiar, os X
 
-        self._fSetFontSize: float = 16  # Wymiar czcionki
         # Listy krotek dla pomiarów referencyjnych i rzeczywistych
         self._myListRef: list = []
         self._myListMeas: list = []
         # Wypełnienie listy krotek dla pomiarów referencyjnych i rzeczywistych
         for i in range(self._iCountRef):
-            self._myListRef.append((self._fOffset + (self._fX * i), self._fHeight + self._fOffset - (self._fYConvRef * self._fTableRef[i])))
-            self._myListMeas.append((self._fOffset + (self._fX * i), self._fHeight + self._fOffset - (self._fYConvRef * self._fTableMeas[i])))
-        # tworzenie dokumentu PDF
+            self._myListRef.append((self._fOffset + (self._fX * i),
+                                    self._fHeight + self._fOffset - (self._fYConvRef * self._fTableRef[i])))
+            self._myListMeas.append((self._fOffset + (self._fX * i),
+                                     self._fHeight + self._fOffset - (self._fYConvRef * self._fTableMeas[i])))
+
+    # Inicjalizacja podstawowych zmiennych dla klasy FPDF
+    def __pdf_init(self):
+        """
+        @return: Brak.
+        """
         self._pdf = fpdf.FPDF(orientation="P", unit="pt", format="A4")
         self._pdf.add_page()
         # Definiowanie czcionek
@@ -53,8 +73,6 @@ class GsPyMyChartBase:
         self._pdf.add_font("TimesB", fname="C:\\Windows\\Fonts\\Timesbd.ttf")
         self._pdf.add_font("TimesBI", fname="C:\\Windows\\Fonts\\Timesbi.ttf")
         # Rysowanie osi X i Y
-        self._pdf.set_font("TimesB", size=self._fSetFontSize)
-        self._pdf.set_text_color(255, 0, 255)
         self._pdf.polyline([(self._fOffset, self._fOffset), (self._fOffset, self._fOffset + self._fHeight),
                             (self._fOffset + self._fWidth, self._fOffset + self._fHeight)])
         # --- Rysowanie siatki ---
@@ -66,7 +84,20 @@ class GsPyMyChartBase:
         # Rysowanie poziomych linii
         for i in range(self._iCountRef):
             self._pdf.polyline([(self._fOffset, self._fHeight + self._fOffset - (self._fYConvRef * self._fTableRef[i])),
-                                (self._fOffset + self._fWidth, self._fHeight + self._fOffset - (self._fYConvRef * self._fTableRef[i]))])
+                                (self._fOffset + self._fWidth,
+                                 self._fHeight + self._fOffset - (self._fYConvRef * self._fTableRef[i]))])
+
+    # Rysowanie tytułu
+    def __paint_title(self):
+        """
+        @return: Brak.
+        """
+        self._fSetFontSize: float = 12  # Wymiar czcionki
+        self._pdf.set_text_color(0, 0, 0)
+        self._pdf.set_fill_color(r=255, g=255, b=0)
+        self._pdf.set_font("TimesN", size=self._fSetFontSize)
+        self._pdf.set_xy(self._fOffset, self._fOffset)
+        self._pdf.cell(w=self._fWidth, h=self._fOffset, txt=self._strTitle, border=1, align="C", fill=True)
 
     # Destruktor klasy bazowej
     def __del__(self):
@@ -82,14 +113,14 @@ class GsPyMyChartBase:
 """
 class GsPyMyChart(GsPyMyChartBase):
     # Konstruktor
-    def __init__(self, fInTableRef: list, fInTableMeas: list, strPathPDF: str):
+    def __init__(self, fInTableRef: list, fInTableMeas: list, strPathPDF: str, strTitle: str = ""):
         """
         @param: fInTableRef: Tablica wyników referencyjnych.
         @param: fInTableMeas: Tablica pomiarów.
         @return: Brak.
         """
         # print("Konstruktor klasy GsPyMyChart")
-        super().__init__(fInTableRef, fInTableMeas)  # Wywołanie konstruktora klasy przodka
+        super().__init__(fInTableRef, fInTableMeas, strTitle)  # Wywołanie konstruktora klasy przodka
         if self._iCountRef > self._iMaxMeas or self._iCountRef != len(self._fTableMeas):
             # Listy wejściowe, a co następuje składowe,  mają różną ilość elementów.
             return
@@ -137,14 +168,14 @@ class GsPyMyChart(GsPyMyChartBase):
 
 class GsPyMyChartBox(GsPyMyChartBase):
     # Konstruktor
-    def __init__(self, fInTableRef: list, fInTableMeas: list, strPathPDF: str):
+    def __init__(self, fInTableRef: list, fInTableMeas: list, strPathPDF: str, strTitle: str = ""):
         """
         @param: fInTableRef: Tablica wyników referencyjnych.
         @param: fInTableMeas: Tablica pomiarów.
         @return: Brak.
         """
         # print("Konstruktor klasy GsPyMyChart")
-        super().__init__(fInTableRef, fInTableMeas)  # Wywołanie konstruktora klasy przodka
+        super().__init__(fInTableRef, fInTableMeas, strTitle)  # Wywołanie konstruktora klasy przodka
         if self._iCountRef > self._iMaxMeas or self._iCountRef != len(self._fTableMeas):
             # Listy wejściowe, a co następuje składowe,  mają różną ilość elementów.
             return
@@ -169,11 +200,10 @@ class GsPyMyChartBox(GsPyMyChartBase):
             self._pdf.set_draw_color(0, 0, 0)
             self._pdf.set_fill_color(0, 0, 255)
             self._pdf.rect(self._myListRef[i][0], self._myListRef[i][1],
-                        10, self._fHeight - self._myListRef[i][1] + self._fOffset, "FD")
+                           10, self._fHeight - self._myListRef[i][1] + self._fOffset, "FD")
             self._pdf.set_draw_color(0, 0, 0)
             self._pdf.set_fill_color(255, 0, 0)
             self._pdf.rect(self._myListMeas[i][0] + 10, self._myListMeas[i][1],
-                        10, self._fHeight - self._myListMeas[i][1] + self._fOffset, "FD")
+                           10, self._fHeight - self._myListMeas[i][1] + self._fOffset, "FD")
 
         self._pdf.output(self.pathPDF)
-
